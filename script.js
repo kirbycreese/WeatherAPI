884a5eff92b75f216816a6cfd1ee197010a48bfb
kirbycreese <70101203+kirbycreese@users.noreply.github.com>
2020-10-11 11:25:33 -0400

    debugging, adding document.ready

---
+++ script.js
@@ -0,0 +1,4 @@
+//document.ready makes sure that everything happens in the right order AND the code goes in between the curly brackets
+$(document ).ready(function() {
+    console.log( "ready!" );
+});
