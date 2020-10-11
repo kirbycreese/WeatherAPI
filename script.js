fbb438c5c6640ca7e30dc90caee7ae3e2068b322
kirbycreese <70101203+kirbycreese@users.noreply.github.com>
2020-10-11 11:38:29 -0400

    comment out first steps

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 //document.ready makes sure that everything happens in the right order AND the code goes in between the curly brackets
 $(document ).ready(function() {
-    console.log( "ready!" );
+ //create click function for search button (call search button by ID
+ //add "preventDefault to prevent data from flashing")
+ //create variables to build URL
+ //var city will be the data key (what gets typed in search bar)
+ //var query will be query URL + city + api key
+ //under that we will need an AJAX function to get data from the API and create variables within for the responses (name of city, temperature, humidity, wind)
 });
